e0412a96b9d0e2e9389823cf9ea52b8335a43c74
Tushar Joshi <tusharjoshi4531@gmail.com>
2023-04-26 19:19:01 +0530

    Fixed get topics api

--- controllers/Discussion/getTopics.ts
+++ controllers/Discussion/getTopics.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import TopicModel from "../../models/topic";
 import { ITopicData } from "../../types/discussion";
+import { Types } from "mongoose";
 
 interface IResponseTopicData extends ITopicData {
     id: string;
@@ -8,20 +9,29 @@ interface IResponseTopicData extends ITopicData {
 
 const getTopics: RequestHandler = async (req, res) => {
     const query = req.query.tags;
+    console.log(query);
+    if (query === undefined)
+        return res.status(404).json({ message: "No Querry found" });
 
     try {
+        let result: (ITopicData & {
+            _id: Types.ObjectId;
+        })[];
         if (query === "all" || query === undefined) {
-            const result = await TopicModel.find();
-
-            const data: IResponseTopicData[] = result.map((topic) => ({
-                title: topic.title,
-                tags: topic.tags,
-                author: topic.author,
-                id: topic._id.toString(),
-            }));
-
-            res.status(200).json(data);
+            result = await TopicModel.find();
+        } else {
+            const tags = JSON.parse(query as string);
+            result = await TopicModel.find({ tags: { $all: tags } });
         }
+
+        const data: IResponseTopicData[] = result.map((topic) => ({
+            title: topic.title,
+            tags: topic.tags,
+            author: topic.author,
+            id: topic._id.toString(),
+        }));
+
+        res.status(200).json(data);
     } catch (error) {
         res.status(404).send("Couldn't get topics");
     }
